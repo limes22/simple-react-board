eb27efb7dfea43786ac0747b821a8fee45f8e759
limes22 <howdi20002@naver.com>
2022-07-03 14:10:40 +0000

    simple-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,79 @@
-import React from 'react';
-import './App.css';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { moveEmitHelpers } from "typescript";
+
 
 function App() {
+  const [movieContent, setMovieContent] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [viewContent, setViewContent] = useState([]);
+
+  const getValue = (e: any) => {
+    const { name, value } = e.target;
+    setMovieContent({
+      ...movieContent,
+      [name]: value,
+    });
+    console.log(movieContent);
+  };
+
   return (
     <div className="App">
       <h1>Movie Review</h1>
-      <div className='movie-container'>
+      <div className="movie-container">
+        {/* {viewContent.map(element =>
+          <div>
+            <h2>{element.title}</h2>
+            <div>
+              {element.content}
+            </div>
+          </div>)} */}
         <h2>제목</h2>
-        <div>
-          내용
-        </div>
+        <div>내용</div>
       </div>
-      <div className='form-wrapper'>
-        <input className="title-input" type='text' placeholder='제목' />
+      <div className="form-wrapper">
+        <input
+          className="title-input"
+          type="text"
+          placeholder="제목"
+          onChange={getValue}
+          name="title"
+        />
         <CKEditor
           editor={ClassicEditor}
           data="<p>Hello from CKEditor 5!</p>"
-          onReady={(editor :any) => {
+          onReady={(editor) => {
             // You can store the "editor" and use when it is needed.
-            console.log('Editor is ready to use!', editor);
+            console.log("Editor is ready to use!", editor);
           }}
-          onChange={(event :any , editor :any) => {
+          onChange={(event, editor) => {
             const data = editor.getData();
             console.log({ event, editor, data });
+            setMovieContent({
+              ...movieContent,
+              content: data,
+            });
+            console.log(movieContent);
           }}
-          onBlur={(event :any, editor :any) => {
-            console.log('Blur.', editor);
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
           }}
-          onFocus={(event :any, editor :any) => {
-            console.log('Focus.', editor);
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
           }}
         />
       </div>
-      <button className="submit-button">입력</button>
+      <button
+        className="submit-button"
+      >
+        입력
+      </button>
     </div>
   );
 }
